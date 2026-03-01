c052ede628e73c4e23f430c0e7b130f646f08f85
Mohammed <mmohammad333333@gmail.com>
2026-03-01 18:15:16 +0300

    AWS S3 Command line

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Docker!"
+    return "Hello, Docker!, this is me form docker hub V2 with sh command V2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5005)
---
+++ myscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Welcome to Docker pushing"
+
+docker build -t flask-app-img .
+
+docker tag flask-app-img malghamdi33/first-docker-hub-flask-img:latest
+
+docker push malghamdi33/first-docker-hub-flask-img:latest
+
+#Remove running conter if exist
+docker rm running-flask-app-from-docker-hub
+
+#pull image from docker hub then run it from them.
+docker run -it -p 5005:5005 --name running-flask-app-from-docker-hub malghamdi33/first-docker-hub-flask-img:latest
+
