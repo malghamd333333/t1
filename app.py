d30089804c502b88b4c1461fc5919873fac36ee7
Mohammed <mmohammad333333@gmail.com>
2026-03-17 02:01:42 +0300

    Testing K8s

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Docker!, this is me form docker hub V2 with sh command V2"
+    return "Hello, Docker! Hello K8s, this is me form docker hub V3 with sh command V3"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5005)
